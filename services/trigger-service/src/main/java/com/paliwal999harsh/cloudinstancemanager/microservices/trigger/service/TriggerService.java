b7642824f77d26b4676c9d3722afcd55fe5585e2
Harsh Paliwal <paliwal.999harsh@gmail.com>
2024-01-30 00:14:18 +0530

    chore

--- services/trigger-service/src/main/java/com/paliwal999harsh/cloudinstancemanager/microservices/trigger/service/TriggerService.java
+++
@@ -1,8 +0,0 @@
-package com.paliwal999harsh.cloudinstancemanager.microservices.trigger.service;
-
-import com.paliwal999harsh.cloudinstancemanager.dto.LeaseDTO;
-
-public interface TriggerService {
-    public void generateStartAndStopActions(LeaseDTO lease);
-    public void performSetStateActions();
-}
